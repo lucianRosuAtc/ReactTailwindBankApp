f27c1a7edce833a025c7a91a0dc0cf53d89bc522
lucianRosuAtc <lucian.rosu.atc@gmail.com>
2023-01-01 23:03:53 +0000

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,10 @@ import styles from '../style'
 import { logo } from '../assets'
 import { footerLinks, socialMedia } from '../constants'
 
+
 const Footer = () => (
   <section className={`${styles.flexCenter} ${styles.paddingY}
-  flex-col`}>
+   flex-col`}>
     <div className={`${styles.flexStart} md:flex-row
      flex-col mb-8 w-full`}>
       <div className='flex-1 flex flex-col justify-start mr-10'>
@@ -16,12 +17,12 @@ const Footer = () => (
       </div>
 
       <div className='flex-[1.5] w-full flex flex-row
-      justify-between flex-wrap md:mt-0 mt-10'>
+       justify-between flex-wrap md:mt-0 mt-10'>
           {footerLinks.map((footerLink) =>(
             <div key={footerLink.key} className="flex flex-col
-            ss:my-0 my-4 min-w-[150px]">
+             ss:my-0 my-4 min-w-[150px]">
               <h4 className='font-poppins font-medium text-[18px]
-              leading-[27px] text-white'>
+               leading-[27px] text-white'>
                 {footerLink.title}
               </h4>
               <ul className='list-none mt-4'>
@@ -41,7 +42,7 @@ const Footer = () => (
     </div>
 
     <div className='w-full flex justify-between items-center
-    md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45]'>
+     md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45]'>
       <p className='font-poppins font-normal text-center
        text-[18px] leading-[27px] text-white'>
         Copyright  2021 HooBank. All Rights Reserved.
